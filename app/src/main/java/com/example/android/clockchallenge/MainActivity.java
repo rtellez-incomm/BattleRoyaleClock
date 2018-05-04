c13383203919c1a5a4de0a19d695285e21e5cafa
rtellez <rtellez@incomm.com>
2018-05-04 10:32:56 -0400

    git

---
+++ app/src/main/java/com/example/android/clockchallenge/MainActivity.java
@@ -0,0 +1,18 @@
+package com.example.android.clockchallenge;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.tomerrosenfeld.customanalogclockview.CustomAnalogClock;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        CustomAnalogClock customAnalogClock = (CustomAnalogClock) findViewById(R.id.analog_clock);
+        customAnalogClock.setAutoUpdate(true);
+    }
+}
